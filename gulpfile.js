fece23cd5b96f1b5e7258cedd9409cd2d23ab67c
Raul <raul.muser@gmail.com>
2022-06-20 23:47:08 +0300

    Add advantage, position and map blocks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 let gulp = require('gulp');
 
 function build() {
-    gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.min.css']).pipe(gulp.dest('./assets/vendor/css/'));
+    gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.min.*']).pipe(gulp.dest('./assets/vendor/css/'));
+    gulp.src(['./node_modules/bootstrap/dist/js/bootstrap.min.*']).pipe(gulp.dest('./assets/vendor/js/'));
 }
 
 exports.build = build;
