c93edf5ceeb3019b01fa1bee4724e4ec149e15d3
Raul <raul.muser@gmail.com>
2022-06-27 07:43:32 +0300

    Add details, contact and animations

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 let gulp = require('gulp');
 
 function build() {
-    gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.min.*']).pipe(gulp.dest('./assets/vendor/css/'));
-    gulp.src(['./node_modules/bootstrap/dist/js/bootstrap.min.*']).pipe(gulp.dest('./assets/vendor/js/'));
+    gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.min.*',
+        './node_modules/aos/dist/aos.css',
+    ]).pipe(gulp.dest('./assets/vendor/css/'));
+    gulp.src([
+        './node_modules/bootstrap/dist/js/bootstrap.min.*',
+        './node_modules/aos/dist/aos.js*',
+    ]).pipe(gulp.dest('./assets/vendor/js/'));
 }
 
 exports.build = build;
